338bfae1abfcf571530357c2f41fb8fd34084655
unknown <shamy_pw4@hotmail.co.uk>
2023-11-01 22:18:36 +0000

    removed unused express app

--- backend/index.jsx
+++ backend/index.jsx
@@ -7,9 +7,6 @@ require("dotenv").config();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
