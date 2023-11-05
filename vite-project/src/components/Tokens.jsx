449861b78452ae739c3202803ecef20841af27d3
unknown <shamy_pw4@hotmail.co.uk>
2023-11-05 20:54:00 +0000

    started work on the code to display retreived erc20 tokens balances on the front end. Need to complete the return and ensure data is being displayed correctly

---
+++ vite-project/src/components/Tokens.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import PropTypes from 'prop-types'
+
+function Tokens({ wallet, chain, tokens, setTokens}) {
+
+    async function getTokenBalances() {
+
+        const response = await axios.get("http://localhost:8080/tokenBalances", {  
+            params: {
+                address: wallet,
+                chain: chain,
+            } 
+    })
+
+    if (response.data) {
+        let tkns = response.data;
+      
+        for (let i = 0; i < tkns.length; i++) {
+          tkns[i].bal = (Number(tkns[i].balance) / Number(`1E${tkns[i].decimals}`)).toFixed(3);
+          tkns[i].value = (Number(tkns[i].balance) / Number(`1E${tkns[i].decimals}`) * Number(tkns[i].usd)).toFixed(2);
+        }
+      
+        setTokens(tkns);
+      }
+      
+
+    }
+  return (
+    <>
+        <p>
+            <button onClick={getTokenBalances}>Get Tokens</button>
+            <br />
+            <span>
+                Tokens: {tokens}
+            </span>
+        </p>
+    </>
+  )
+}
+
+export default Tokens
