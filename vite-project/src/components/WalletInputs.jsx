68c5098f10e84d66264823eb782bcc4dd379ef84
ShamillFazal <shamy_pw4@hotmail.co.uk>
2023-11-23 10:54:19 +0000

    started with styling, imported tailwind css & web3ui. Added basic code for header. Need to add logo. Also added a coonnect button to the header in case of future web3 wallet connectivity

--- vite-project/src/components/WalletInputs.jsx
+++ vite-project/src/components/WalletInputs.jsx
@@ -3,6 +3,14 @@ import PropTypes from "prop-types";
 function WalletInputs({ wallet, setWallet, chain, setChain }) {
   return (
     <>
+    <header className="bg-white border-b border-gray-300 p-4 flex items-center justify-between">
+      <div className="flex items-center">
+        <div className="text-2xl font-bold mr-4">Your Logo</div>
+      </div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Connect
+      </button>
+    </header>
       <h1>Enter wallet & chain address</h1>
       <p>
         <span>Wallet Address </span>
